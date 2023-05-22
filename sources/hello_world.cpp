a63f146eef5f39be7856fc2f7ef985dfb4ac0f40
SplashFan <salruslan@mail.ru>
2023-05-22 10:30:24 +0300

    changed code-style

--- sources/hello_world.cpp
+++ sources/hello_world.cpp
@@ -2,10 +2,12 @@
 
 using namespace std;
 
-int main(){
-	string name;
-	cout<<"Enter your name";
-	getline(cin, name);
-	cout<<"Hello, world from "<<name;
-return 0;
+int
+main()
+{
+  string name;
+  cout << "Enter your name";
+  getline(cin, name);
+  cout << "Hello, world from " << name;
+  return 0;
 }
